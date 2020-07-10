58e7ab1ff9499e94b8b8d4c0f82a1a1235b29750
Erik <ecowley@protonmail.com>
2020-07-10 11:35:28 -0400

    finished ex1 and ex2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,21 @@
+from collections import defaultdict
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    if len(weights) <= 1:
+        return None
+
+    weight_to_ix = defaultdict(lambda: [])
+    for ix, w in enumerate(weights):
+        weight_to_ix[w].append(ix)
+
+    for w in weights:
+        if limit-w in weight_to_ix:
+            if limit-w == w and len(weight_to_ix[w]) >= 2:
+                return (weight_to_ix[w][1], weight_to_ix[w][0])
+            elif limit-w != w:
+                return (weight_to_ix[limit-w][0], weight_to_ix[w][0])
 
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,26 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # avoid potential headaches for small input size
+    if len(tickets) == 0:
+        return []
+
+    if len(tickets) == 1:
+        return [tickets[0].source]
+
+    # construct a dict of source-to-destinations
+    s2d = {}
+    for ticket in tickets:
+        s2d[ticket.source] = ticket.destination
+
+    # first is labeled "NONE"
+    source = s2d["NONE"]
+
+    # final will be labeled "NONE" for dest
+    route = []
+    while source != "NONE":
+        route.append(source)
+        source = s2d[source]
+    route.append(source)
 
     return route
